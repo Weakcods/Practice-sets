c29bbe8c550184e6303548981d739d613a374db4
noe <2020100612bp@psu.palawan.edu.ph>
2025-02-18 12:29:42 +0800

    add matrix processing to display even numbers from a 3x3 matrix

--- practice3/array.php
+++ practice3/array.php
@@ -7,7 +7,31 @@
 </head>
 <body>
     <h2>EVEN NUMBERS</h2>
-    
+    <?php
+    // Define a 3x3 matrix
+    $matrix = [
+        [12, 23, 34],
+        [45, 55, 62],
+        [71, 84, 90]
+    ];
+
+    // Initialize row and column counters
+    $row = 0;
+
+    echo "Even numbers from the matrix:<br>";
+
+    // Iterate through the matrix using a while loop
+    while ($row < count($matrix)) {
+        $col = 0;
+        while ($col < count($matrix[$row])) {
+            if ($matrix[$row][$col] % 2 == 0) {
+                echo $matrix[$row][$col] . "<br>";
+            }
+            $col++;
+        }
+        $row++;
+    }
+?>
     
 </body>
 </html>
